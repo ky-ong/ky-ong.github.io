3717c764379747bd3abcfa439bc065dc9c73a027
Ong Kang Yaw <ongkangyaw@ongs-Mac-mini.local>
2015-11-13 18:10:27 +0800

    iiii

--- test2.sh
+++ test2.sh
@@ -1,6 +1,6 @@
 
 while true; do
-	ping -t 3 google.com | awk -v host=google.com -v date="$(date)" '/round-trip/ {print date, host, $4}' >> ms.txt
+	ping -t 3 google.com | awk -v host=google.com -v date="$(date)" '/round-trip/ {print date, "|", host, "|", $4}' >> stdms.txt
 	sleep 5
 done
 
